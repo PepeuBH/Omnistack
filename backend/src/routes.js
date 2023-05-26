1c51d6db2dda7984795b6e20dcab2ffdc8cc54d8
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-05-26 11:21:00 -0300

    Update - routes.js + module (cors)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,16 @@ const OngController = require('./controllers/OngController');
 
 const IncidentController = require('./controllers/IncidentController');
 
+const ProfileController = require('./controllers/ProfileController');
+
+const SessionController = require('./controllers/SessionController');
+
+
 const routes = express.Router();
 
 
+//rota para CRIAR uma sessão
+routes.post('/sessions', SessionController.create);
 
 //rota com CONSULTAR SQL para lista todas as ongs do BD
 routes.get('/ongs', OngController.list);
@@ -22,6 +29,10 @@ routes.get('/incidents', IncidentController.list);
 
 //rota para DELETAR um caso de uma ong
 routes.delete('/incidents/:id', IncidentController.delete);
+
+//rota para listar um caso só
+routes.get('/profile', ProfileController.list);
+
    
 
 
